bbae8a1902baddd75828c9101bcfe85a5096ad27
elenalagroteria <elenalagroteria@gmail.com>
2025-12-05 16:20:02 -0300

    começando o footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,69 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <footer>
+    <section class="infos">
+    <div>
+        <article>
+            <h5>Contato</h5>
+            <a href="mailto:contato@inspiralens.com">contato@inspiralens.com</a>
+            <a href="">Central de ajuda</a>
+            <p>Rio de Janeiro - Brasil</p>
+        </article>
+        <article>
+            <h5>Sobre</h5>
+            <a href="">Políticas</a>
+            <a href="">Privacidade</a>
+            <a href="">Blog</a>
+        </article>
+    </div>
+        <article>
+            <h5>Assine nossa newsletter</h5>
+            <input type="text" id="newsletter" name="newsletter" placeholder="Seu melhor e-mail" required>
+            <button type="submit">Submit</button>
+        </article>
+    </section>
+
+    <section class="redes">
+
+    </section>
+</footer>
+</template>
+
+
+<style scoped lang="scss">
+footer {
+  display: flex;
+  justify-content: space-between;
+  margin: 2rem;
+  text-align: center;  
+
+    .infos {
+        display: flex;
+        flex-direction: column-reverse;
+        justify-content: center;
+
+        h5 {
+            font-size: 5vw;
+        }
+
+        p {
+            font-size: 4vw;
+        }
+
+        a {
+            font-size: 4vw;
+        }
+    }
+
+    article {
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        padding: 1vh 0;
+    }
+}
+</style>
---
+++ src/components/Inspire.vue
@@ -0,0 +1,21 @@
+<script setup>  
+import Card from "./Card.vue";
+</script>
+
+<template> 
+    <section>
+        <h2>Inspire-se</h2>
+    <Card imagem="https://invexo.com.br/blog/wp-content/uploads/2020/06/apartamentos-na-praia-de-s%C3%A3o-conrado-rio-de-janeiro-rj.jpg"  
+    />
+    <Card imagem="https://invexo.com.br/blog/wp-content/uploads/2020/06/apartamentos-na-praia-de-s%C3%A3o-conrado-rio-de-janeiro-rj.jpg"  
+    />
+    <Card imagem="https://invexo.com.br/blog/wp-content/uploads/2020/06/apartamentos-na-praia-de-s%C3%A3o-conrado-rio-de-janeiro-rj.jpg"  
+    />
+    <Card imagem="https://invexo.com.br/blog/wp-content/uploads/2020/06/apartamentos-na-praia-de-s%C3%A3o-conrado-rio-de-janeiro-rj.jpg"  
+    />
+    </section>
+
+
+</template>
+
+<style scoped lang="scss"> </style>
